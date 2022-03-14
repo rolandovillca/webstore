6f1787bdf22e1f3c733fa565a4dc3454e461e877
Sahil Maharjan <sahilzmaharjan@Sahils-MacBook-Pro.local>
2022-03-14 17:04:47 -0500

    product model added

---
+++ shoppingCartCommandService/src/main/java/shoppingCartCommandService/shoppingcartcommandservice/model/Product.java
@@ -0,0 +1,60 @@
+package shoppingCartCommandService.shoppingcartcommandservice.model;
+
+public class Product {
+    private int product_no;
+    private String name;
+    private Double price;
+    private String description;
+    private int no_in_stock;
+
+    public Product() {
+    }
+
+    public Product(int product_no, String name, Double price, String description, int no_in_stock) {
+        this.product_no = product_no;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.no_in_stock = no_in_stock;
+    }
+
+    public int getProduct_no() {
+        return product_no;
+    }
+
+    public void setProduct_no(int product_no) {
+        this.product_no = product_no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNo_in_stock() {
+        return no_in_stock;
+    }
+
+    public void setNo_in_stock(int no_in_stock) {
+        this.no_in_stock = no_in_stock;
+    }
+}
