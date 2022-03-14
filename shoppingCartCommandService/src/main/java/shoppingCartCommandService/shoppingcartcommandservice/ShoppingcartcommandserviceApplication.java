1ad584acfa75ae6407935266b4838c3ee2059dc3
Sahil Maharjan <sahilzmaharjan@Sahils-MacBook-Pro.local>
2022-03-13 20:37:50 -0500

    initial commit with shopping cart command service created

---
+++ shoppingCartCommandService/src/main/java/shoppingCartCommandService/shoppingcartcommandservice/ShoppingcartcommandserviceApplication.java
@@ -0,0 +1,13 @@
+package shoppingCartCommandService.shoppingcartcommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartcommandserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartcommandserviceApplication.class, args);
+	}
+
+}
---
+++ shoppingCartCommandService/src/test/java/shoppingCartCommandService/shoppingcartcommandservice/ShoppingcartcommandserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package shoppingCartCommandService.shoppingcartcommandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartcommandserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
