8ce3d70004545497e9644c6ec1174e00cb67b6a3
szn1995 <sujan.koju.sastra@gmail.com>
2022-03-14 10:29:12 -0500

    topic config added

---
+++ order-service/src/main/java/com/webstore/orderservice/events/OrderEvent.java
@@ -0,0 +1,8 @@
+package com.webstore.orderservice.events;
+
+/**
+ * @author kojusujan1111@gmail.com 14/03/22
+ */
+
+public interface OrderEvent {
+}
--- order-service/src/main/java/com/webstore/orderservice/listners/CreateOrderListener.java
+++ order-service/src/main/java/com/webstore/orderservice/listners/CreateOrderListener.java
@@ -2,12 +2,16 @@ package com.webstore.orderservice.listners;
 
 import com.webstore.orderservice.domains.Order;
 import com.webstore.orderservice.events.CreateOrderEvent;
+import com.webstore.orderservice.events.OrderEvent;
+import com.webstore.orderservice.events.OrderPlacedEvent;
 import com.webstore.orderservice.repositories.OrderRepository;
+import com.webstore.orderservice.senders.KafkaMessageSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
@@ -15,21 +19,26 @@ import java.util.UUID;
  * @author kojusujan1111@gmail.com 14/03/22
  */
 
-
-@Component
+@Service
 public class CreateOrderListener {
+    @Value("${create_order_topic:create-order-topic}")
+    private final String createOrderTopic = "";
+
     Logger logger = LoggerFactory.getLogger(CreateOrderListener.class);
     private final OrderRepository orderRepository;
+    private final KafkaMessageSender<OrderEvent> kafkaMessageSender;
 
-    public CreateOrderListener(OrderRepository orderRepository) {
+    public CreateOrderListener(OrderRepository orderRepository, KafkaMessageSender<OrderEvent> kafkaMessageSender) {
         this.orderRepository = orderRepository;
+        this.kafkaMessageSender = kafkaMessageSender;
     }
 
-    @KafkaListener(topics = "create_order_topic")
+    @KafkaListener(topics = createOrderTopic)
     public void receive(@Payload CreateOrderEvent createOrderEvent) {
         logger.info("-- Create Order Message Received {} -- ", createOrderEvent);
         Order order = prepareOrder(createOrderEvent);
         orderRepository.save(order);
+        kafkaMessageSender.dispatchOrderPlacedEvent(new OrderPlacedEvent(order.getOrderId(), order.getCustomerId(), order.getShippingCartId()));
     }
 
     private Order prepareOrder(CreateOrderEvent createOrderEvent) {
