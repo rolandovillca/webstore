4a5205de6a974167d3d6c08ed75d5c68796551df
Sahil Z Maharjan <sahil.maharjan678@gmail.com>
2022-03-16 11:24:11 -0500

    load balancer added on shoppingcart command service

--- shoppingCartCommandService/src/main/java/shoppingCartCommandService/shoppingcartcommandservice/service/ProductQueryService.java
+++ shoppingCartCommandService/src/main/java/shoppingCartCommandService/shoppingcartcommandservice/service/ProductQueryService.java
@@ -1,11 +1,14 @@
 package shoppingCartCommandService.shoppingcartcommandservice.service;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shoppingCartCommandService.shoppingcartcommandservice.configs.LoadBalancerConfiguration;
 import shoppingCartCommandService.shoppingcartcommandservice.model.Product;
 
 @FeignClient(name = "product-service")
+@LoadBalancerClient(name = "product-service", configuration = LoadBalancerConfiguration.class)
 public interface ProductQueryService {
 
     @RequestMapping("/products/{productId}")
